3d506bb7a0c0bcf3f76e0e2e5c2687901379d609
Stefano Cislaghi <s.cislaghi@gmail.com>
2025-02-10 20:25:12 +0000

    choreo: prettier

--- api/minimet.ts
+++ api/minimet.ts
@@ -4,20 +4,19 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   const minimetUrl = process.env.MINIMET_URL || "";
   const token = process.env.MINIMET_TOKEN || "";
 
-  const headers = { 
-    'Authorization': `Bearer ${token}`,
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-    'Accept':'application/json',
-   };
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    "User-Agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    Accept: "application/json",
+  };
 
-  fetch(minimetUrl, {headers, method: "POST"})
+  fetch(minimetUrl, { headers, method: "POST" })
     .then((res) => res.json())
     .then((data) => {
       return res.status(200).json(data);
     })
     .catch((err) => {
-      return res.status(500).json({error: err});
+      return res.status(500).json({ error: err });
     });
-
-  
 }
